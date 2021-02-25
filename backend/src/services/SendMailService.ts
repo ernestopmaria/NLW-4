efe8436cfb712182aeacaadc2baa1ac9429722f7
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-02-25 17:49:38 +0100

    ethereal

--- backend/src/services/SendMailService.ts
+++ backend/src/services/SendMailService.ts
@@ -1,5 +1,34 @@
+import nodemailer, { Transporter } from 'nodemailer'
+
 
 class SendMailService {
+    private client: Transporter
+    constructor() {
+        nodemailer.createTestAccount().then(account => {
+            const transporter = nodemailer.createTransport({
+                host: "smtp.ethereal.email",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: account.user, // generated ethereal user
+                    pass: account.pass, // generated ethereal password
+                },
+            });
+
+            this.client = transporter
+        })
+    }
+
+    async execute(to: string, subject: string) {
+        await this.client.sendMail({
+            from: '"Fred Foo 👻" <foo@example.com>',
+            to: "bar@example.com, baz@example.com",
+            subject: "Hello ✔",
+            text: "Hello world?",
+            html: "<b>Hello world?</b>",
+        });
+
+    }
 
 
 }
