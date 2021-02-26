777c99993a54ec058af213e4b8ef9f6bb6a9b4c9
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-02-26 16:54:15 +0100

    Update AnswerController.ts

--- backend/src/controller/AnswerController.ts
+++ backend/src/controller/AnswerController.ts
@@ -1,8 +1,21 @@
-/* class AnswerController {
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { SurveyUserRepository } from '../repositories/SurveyUserRepository';
+
+class AnswerController {
 
     async execute(request: Request, response: Response) {
         const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveyUserRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u)
+        });
+
+        if (!surveyUser)
 
     }
 
-} */
+}
