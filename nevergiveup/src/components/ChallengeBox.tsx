7ad395f2216a5cc0d4af132a73d33c7174785690
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-02-26 13:32:10 +0100

    update

--- nevergiveup/src/components/ChallengeBox.tsx
+++ nevergiveup/src/components/ChallengeBox.tsx
@@ -1,19 +1,43 @@
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
+
+    const hasActiveChallenge = true
     return (
         <div className={styles.containerChallengeBox}>
-            <div>
-                <strong className={styles.challengeNotActive}>
-                    Finalize um ciclo para receber um desafio!</strong>
-                <p >
-                    <img src="icons/level.svg" alt="Level Up" />
+            {hasActiveChallenge ? (<div className={styles.challengeActive}>
+
+                <header>
+                    Ganhe 400xp
+                </header>
+                <main>
+                    <img src="icons/body.svg" alt="Body" />
+                    <strong>Novo desafio</strong>
+                    <p>Levante e faça uma caminhada de 3 minutos</p>
+                </main>
+
+                <footer>
+                    <button type="button" className={styles.challengeFailed}>Falhei </button>
+                    <button type="button" className={styles.challengeCompleted}>Completei </button>
+
+
+
+                </footer>
+            </div>) :
+
+
+                (<div className={styles.challengeNotActive}>
+                    <strong >
+                        Finalize um ciclo para receber um desafio!</strong>
+                    <p >
+                        <img src="icons/level-up.svg" alt="Level Up" />
                         Avance de level complentando desafios.
                     </p>
 
-            </div>
+                </div>)
+            }
 
-        </div>
+        </div >
     )
 
 
